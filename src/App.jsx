e6bed5b2b56679190c94a9c522320ec4339b5311
Sri Vishnu <srivishp@gmail.com>
2023-08-22 22:07:06 +0530

    Added a custom http hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,28 @@
 import React, { useEffect, useState } from "react";
-
+import useHttp from "./hooks/use-http";
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-tasks-f5c4d-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
-      );
+  const transformTasks = (taskObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  // giving alias as fetchTasks as it is used with that name in this file
+  const httpData = useHttp({
+    url: "https://react-tasks-f5c4d-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+    transformTasks,
+  });
+
+  const { isLoading, error, sendRequest: fetchTasks } = httpData;
 
   useEffect(() => {
     fetchTasks();
